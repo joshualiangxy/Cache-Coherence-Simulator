5a4e248d9734aa0deb8cdbc3133f239257da8e39
joshualiangxy <joshualiang.xy@gmail.com>
2022-11-07 05:27:35 +0800

    Add script for running MESI and Dragon protocols

--- execute.sh
+++ execute.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
 
-for benchmark in blackscholes bodytrack fluidanimate;
+for protocol in MESI MESIF Dragon;
 do
-    for i in 64 128 256 512 1024 2048 4096 8192 16384;
+    for benchmark in blackscholes bodytrack fluidanimate;
     do
-        ./coherence MESIF $benchmark $i 2 32 >> logs/$benchmark-cachesize-MESIF;
+        for i in 64 128 256 512 1024 2048 4096 8192 16384;
+        do
+            echo "$protocol $benchmark - Cache Size: $i";
+            ./coherence $protocol $benchmark $i 2 32 >> logs/$benchmark-cachesize-$protocol;
+        done;
     done;
 done
+
+for protocol in MESI MESIF Dragon;
+do
+    for benchmark in blackscholes bodytrack fluidanimate;
+    do
+        for i in 1 2 4 8 16 32 64 128;
+        do
+            echo "$protocol $benchmark - Associativity: $i";
+            ./coherence $protocol $benchmark 4096 $i 32 >> logs/$benchmark-associativity-$protocol;
+        done;
+    done;
+done
+
+for protocol in MESI MESIF Dragon;
+do
+    for benchmark in blackscholes bodytrack fluidanimate;
+    do
+        for i in 4 8 16 32 64 128 256 512 1024 2048;
+        do
+            echo "$protocol $benchmark - Block Size: $i";
+            ./coherence $protocol $benchmark 4096 2 $i >> logs/$benchmark-blocksize-$protocol;
+        done;
+    done;
+done
+
